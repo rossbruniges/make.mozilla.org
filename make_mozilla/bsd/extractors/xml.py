c41433619574712302709244fe0469bc49fecd82
Ross Bruniges <rossbruniges@gmail.com>
2013-04-02 13:27:26 +0100

    Some fixes to get the importer working again

--- make_mozilla/bsd/extractors/xml.py
+++ make_mozilla/bsd/extractors/xml.py
@@ -1,5 +1,10 @@
 from lxml import etree
+parser = etree.XMLParser(recover=True)
+
 
 def constituent_email(xml):
-    doc = etree.fromstring(xml)
+    print '### XML ###'
+    print xml
+    print '###'
+    doc = etree.fromstring(xml, parser)
     return doc.xpath('/api/cons/cons_email/email')[0].text
